43a09eb392ea95233e94bf1f07af100d76448444
henloboi <henloboii@protonmail.ch>
2020-08-23 21:53:34 +0800

    move fixes to specific dir + specify it's wip

--- AB/fixes.sh
+++ AB/fixes.sh
@@ -15,9 +15,9 @@ echo " 0 : exit "
 read -p "Apply Fix : " fix
 
 if [[ $fix = 1 ]]; then
-   bash prebuilt/oosdata.sh
+   bash prebuilt/oosdata/oosdata.sh
 elif [[ $fix = 2 ]]; then
-   bash prebuilt/vosround.sh
+   bash prebuilt/vosround/vosround.sh
 elif [[ $fix = 3 ]]; then
      bash make2.sh
 fi

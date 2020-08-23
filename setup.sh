b88365cb8fb69fecbbe1210c1a5a86c97df22804
JamieHoSzeYui <you@example.com>
2020-08-23 15:58:56 +0800

    setup : add figlet and fix manjaro install

--- setup.sh
+++ setup.sh
@@ -6,14 +6,19 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" ]]; then
        echo "Arch Linux Detected"
-       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2 img2simg brotli simg2img
+       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2 img2simg brotli figlet 
        #aur=rar
+       https://aur.archlinux.org/simg-tools.git temp
+       cd temp
+       makepkg -si --skippgpcheck
+       cd ..
+       rm -rf temp
     else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 brotli img2simg simg2img
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 brotli img2simg simg2img figlet
     fi
     pip install backports.lzma protobuf pycrypto
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4 aria2
+    brew install protobuf xz brotli lz4 aria2 img2simg simg2img figlet 
     pip install backports.lzma protobuf pycrypto
 fi
 

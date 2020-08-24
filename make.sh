7d21e372a12fbab9b28faae005d3e16ef04bb348
henloboi <henloboii@protonmail.ch>
2020-08-24 12:57:55 +0800

    AO : Fixup

--- make.sh
+++ make.sh
@@ -10,7 +10,7 @@ read -p "Please place GSI inside input folder. press any key to continue."
 if [[ $( ls input/ | grep AB) ]]; then
 	echo "AB detected. Moving to AB script"
 	bash ab.sh
-elif [[ $(ls input/ | grep Aonly)]]; then
+elif [[ $(ls input/ | grep Aonly) ]]; then
 	echo "A Only detected, moving to A script"
 	bash ao.sh
 else 

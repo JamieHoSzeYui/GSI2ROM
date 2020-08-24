62284d444eb227cfb30d15f6618276eae0741a27
henloboi <henloboii@protonmail.ch>
2020-08-24 12:50:29 +0800

    AB : Fixup

--- ab.sh
+++ ab.sh
@@ -14,14 +14,14 @@ sudo dd if=/dev/zero bs=1M count=50 >> cache/system.img
 sudo e2fsck -f cache/system.img
 sudo resize2fs cache/system.img
 read -p "Resize Image? (Y/N)" resize
-if [[ $resize = Y]]; then
+if [[ ($resize = Y) ]]; then
 	read -p "Size you want to add? Use - for decrease" size
 	sudo dd if=/dev/zero bs=1M count=$size >> cache/system.img
         sudo e2fsck -f cache/system.img
         sudo resize2fs cache/system.img
         echo "Resize done !"	
 
-elif [[ $fix = y ]]; then
+elif [[ ($resize = y) ]]; then
 	read -p "Size you want to increase? Use - for decrease" size
 	sudo dd if=/dev/zero bs=1M count=$size >> cache/system.img
 	sudo e2fsck -f cache/system.img
@@ -33,9 +33,9 @@ sudo mount cache/system.img system
 
 read -p "Apply Fixes (Y/N)? : " fix
 
-if [[ $fix = Y]]; then
+if [[ ($fix = Y)]]; then
    bash prebuilt/fixab.sh
-elif [[ $fix = y ]]; then
+elif [[ ($fix = y) ]]; then
    bash prebuilt/fixab.sh
 else 
    bash repack.sh

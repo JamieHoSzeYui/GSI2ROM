c14cebeebb30f7df66133e16a1de19c24418f02c
henloboi <henloboii@protonmail.ch>
2020-08-24 19:38:58 +0800

    Fix Python-pip dependency

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" ]]; then
        echo "Arch Linux Detected"
-       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2 img2simg brotli figlet 
+       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python3-pip brotli lz4 gawk libmpack aria2 img2simg brotli figlet 
        #aur=rar
        https://aur.archlinux.org/simg-tools.git temp
        cd temp
@@ -14,9 +14,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
        cd ..
        rm -rf temp
     else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 brotli img2simg simg2img figlet
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool gawk aria2 brotli img2simg simg2img figlet
     fi
-    pip install backports.lzma protobuf pycrypto
+    pip3 install backports.lzma protobuf pycrypto
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2 img2simg simg2img figlet 
     pip install backports.lzma protobuf pycrypto

b62bf9ce05e0524cfad711713c7d7d941af1575d
JamieHoSzeYui <you@example.com>
2020-08-25 08:21:21 +0800

    Fix : Missing updater

--- repack.sh
+++ repack.sh
@@ -7,6 +7,7 @@ mv cache/system.img cache/system.img.raw
 img2simg cache/system.img.raw cache/system.img
 ./tools/img2sdat/img2sdat.py cache/system.img -o temp -v 4 
 rm -rf cache
+cp -r prebuilt/scripts/normal/ temp
 cd temp
 brotli -3jf system.new.dat
 read -p "Name your zip : " zipname
